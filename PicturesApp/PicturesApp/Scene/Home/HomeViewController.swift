f7fa1116ed85d4bffc509ef260ecdc20009d29ef
Viviana <vivianacapolvenere@gmail.com>
2021-01-14 11:03:22 +0100

    added collection

--- PicturesApp/PicturesApp/Scene/Home/HomeViewController.swift
+++ PicturesApp/PicturesApp/Scene/Home/HomeViewController.swift
@@ -8,9 +8,37 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    lazy private var photoCollection: UICollectionView = {
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collection.register(PhotoCollectionViewCell.self)
+        return collection
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+    }
+    
+    func createLayout() -> UICollectionViewLayout {
+        
+        // let numberOfItemsInRow = UIDevice.current.userInterfaceIdiom == .pad ? 3 : 2
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .absolute(150))
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                       subitem: item, count: 2)
+        
+        group.interItemSpacing = .fixed(CGFloat(10))
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
     }
 }
---
+++ PicturesApp/PicturesApp/Scene/Home/PhotoCell/PhotoCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PicturesApp
+//
+//  Created by Viviana Capolvenere on 14/01/21.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell, Reusable {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+       
+    }
+}
